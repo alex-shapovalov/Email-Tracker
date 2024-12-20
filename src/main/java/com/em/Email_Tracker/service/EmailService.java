6ed12c2f50939877c69a5c250bd76faa0cfe9761
alex-shapovalov <shapovalov.alex@pm.me>
2024-12-20 18:40:10 -0500

    Enhanced secret protection

--- src/main/java/com/em/Email_Tracker/service/EmailService.java
+++ src/main/java/com/em/Email_Tracker/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.em.Email_Tracker.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,13 +11,20 @@ import java.time.LocalDateTime;
 @Service
 public class EmailService {
 
+    @Value("${spring.mail.username}")
+    private String username;
+
+    // Modify this to suit your needs
+    String recipient = "contact@emperor.llc";
+
     @Autowired
     private JavaMailSender mailSender;
 
     public void sendReadReceipt(String recipientEmail, String userAgent, String ipAddress, LocalDateTime timestamp) {
+
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("noreply@emperor.llc");
-        message.setTo("contact@emperor.llc");
+        message.setFrom(username);
+        message.setTo(recipient);
         message.setSubject("Email Read Notification");
         message.setText(String.format(
                 "The email sent to %s was read at %s.\nUser Agent: %s\nIP Address: %s",
